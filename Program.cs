6b5769db448d3b30035361756fffef3b5d4aafbf
Sergey Zryashchev <zryashchev@gmail.com>
2024-02-07 13:56:38 +0400

    Initial commit-1

--- Program.cs
+++ Program.cs
@@ -1,50 +1,76 @@
-﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+﻿/*
+Задайте значения M и N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+*/
+/*
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N:");
+int n = Convert.ToInt32(Console.ReadLine());
+void che(int m, int n)
+{
+if (m > n)
+return;
+if (m % 2 == 0)
+{
+Console.Write($"{m}, ");
+}
+che(m+1,n);
 
-// Console.Write("Введите значение M: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите значение N:");
-// int n = Convert.ToInt32(Console.ReadLine());
-// void che(int m, int n)
-// {
-// if (m > n)
-// return;
-// if (m % 2 == 0)
-// {
-// Console.Write($"{m}, ");
-// }
-// che(m+1,n);
+}
+che(m,n);
+*/
 
-// }
-// che(m,n);
+/*
+Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
 
+/*
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+AkkermanFunction(m,n);
 
-//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
 
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+*/
 
-// {
-//         Console.WriteLine(Akkerman(7, 12));
-//         }
-
-//         int Akkerman(int n, int m) 
-//         {
-//             if (n < 0 || m < 0) throw new ArgumentOutOfRangeException();
-//             if (n == 0) return m + 1;
-//             if (m == 0) return Akkerman(n - 1, m);
-//             return Akkerman(n - 1, Akkerman(n, m - 1));
-//         }
-
-
-
-//Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
-
+/*
+Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+Использовать рекурсию, не использовать циклы.
+*/
 
 char[] charArray = "Hello world!".ToCharArray();
-        {
-        Console.WriteLine(GetReversArray(charArray.Length-1, charArray));
-        }
+    {
+    Console.WriteLine(GetReversArray(charArray.Length-1, charArray));
+    }
 
-        string GetReversArray(int index, char[] array)
-        {
-            if(index < 0) return "";
-            return array[index] + GetReversArray(--index, array);
-        }
+    string GetReversArray(int index, char[] array)
+    {
+        if(index < 0) return "";
+        return array[index] + GetReversArray(--index, array);
+    }
